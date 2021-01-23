6d1f2b8aae10ac55db5f6b11d195560e9401f111
subhanzakizada <zakizadasubhan@gmail.com>
2021-01-23 11:07:20 -0800

    manipulate DOM with JS

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
+// Elements
+const quoteContainer = document.getElementById('quote-container')
+const quoteTextEl = document.getElementById('quote')
+const quoteAuthorEl = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
 // Get Quote from API
 async function getQuote() {
-    // this URL prevents error while accessing to API - which is common for free APIs 
+    // this URL prevents error while accessing the API - which is common for free APIs because of the traffic
     const proxyURL = 'https://cors-anywhere.herokuapp.com/'
-    
+
     // API url
     const quoteURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'
     try {
-        const response = await fetch(quoteURL)
+        const response = await fetch(proxyURL + quoteURL)
         const data = await response.json()
+
+        // if the quote is long, add "long-quote-text" class
+        if (data.quoteText.length > 120) {
+            quoteTextEl.classList.add('long-quote')
+        } else {
+            quoteTextEl.classList.remove('long-quote')
+        }
+        quoteTextEl.innerText = data.quoteText
+        
+        // if author is blank, set to "Unknown"
+        if(data.quoteAuthor === '') {
+            quoteAuthorEl.innerText = 'Unknown'
+        } else {
+            quoteAuthorEl.innerText = data.quoteAuthor
+        }        
+        
     } catch (error) {
-        console.log(`whoops, no quote: ${error}`)
+        getQuote()
     }
 }
 
+// Tweet Quote
+const tweetQuote = function() {
+    const twitterURL = 'https://twitter.com/intent/tweet'
+    const quote = quoteTextEl.innerText
+    const author = quoteAuthorEl.innerText
+    
+    window.open(`${twitterURL}?text=${quote} - ${author}`, '_blank')
+}
+
+// On Load
 getQuote()
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
